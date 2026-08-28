645bea21c9d548af3356925ac666b7148e22dc15
Alfonso <allacalle@gmail.com>
2026-08-28 12:49:37 +0200

    Imagenes hasta windeerdoff 24-28 ahora si

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    totalPages: 23,          // Cambia al número total de imágenes que tengas
+    totalPages: 28,          // Cambia al número total de imágenes que tengas
     imageFolder: 'images/journal/', // Corregido a la ruta de tu carpeta de imágenes
     imageExtension: '.png',
     animationDuration: 350
