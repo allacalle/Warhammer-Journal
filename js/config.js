d60c63e59db9d0ad0f549dfbde73aca3ebb8cd42
Alfonso <allacalle@gmail.com>
2026-09-05 11:11:15 +0200

    Imagenes hasta la 33, winderdoff antes del castillo

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    totalPages: 28,          // Cambia al número total de imágenes que tengas
+    totalPages: 33,          // Cambia al número total de imágenes que tengas
     imageFolder: 'images/journal/', // Corregido a la ruta de tu carpeta de imágenes
     imageExtension: '.png',
     animationDuration: 350
